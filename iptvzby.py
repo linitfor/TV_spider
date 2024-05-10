9ee6474ec0a0fb8611a75c225ff9df06fc60d9dd
outcastveron <39554024+outcastveron@users.noreply.github.com>
2024-05-10 11:40:47 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -679,7 +679,7 @@ with open("bj.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
-    file.write('\北京频道,#genre#\n')
+    file.write('\n北京频道,#genre#\n')
     for result in resultxs:
         channel_name, channel_url = result
         if '北京' in channel_name:
@@ -1017,9 +1017,161 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
+# 扫源凤凰卫视
+
+# 线程安全的队列，用于存储下载任务
+task_queue = Queue()
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+
+urls = ["chengdu", "mianyang", "guangyuan", "deyang", "nanchong", "guangan", "suining", "neijiang", "leshan", "zigong", "luzhou", "yibin", "panzhihua", "bazhong", "dazhou", "ziyang", "meishan", "yaan",
+    "lanzhou", "jiayuguan", "jinchang", "baiyin", "tianshui", "wuwei", "zhangye", "pingliang", "jiuquan", "qingyang", "dingxi", "longnan",
+    "hangzhou", "ningbo", "wenzhou", "jiaxing", "huzhou", "shaoxing", "jinhua", "quzhou", "zhoushan", "taizhou", "lishui",
+    "fuzhou", "xiamen", "zhangzhou", "quanzhou", "sanming", "putian", "nanping", "longyan", "ningde"]
+channelsx = [
+        "凤凰中文,http://8.8.8.8:8/rtp/239.93.1.9:2192",
+    "凤凰资讯,http://8.8.8.8:8/rtp/239.93.1.4:2191",
+    "凤凰香港,http://8.8.8.8:8/udp/239.255.30.123:8231",
+    "凤凰中文,http://8.8.8.8:8/udp/239.255.30.50:8231",
+    "凤凰资讯,http://8.8.8.8:8/udp/239.255.30.70:8231",
+    "凤凰中文,http://8.8.8.8:8/udp/233.50.200.42:5140",
+    "凤凰中文,http://8.8.8.8:8/udp/233.50.200.191:5140",
+    "凤凰资讯,http://8.8.8.8:8/udp/233.50.200.192:5140",
+    "凤凰资讯,http://8.8.8.8:8/udp/239.76.246.8:1234",
+    "凤凰中文,http://8.8.8.8:8/udp/239.76.246.7:1234",
+    "凤凰资讯,http://8.8.8.8:8/rtp/239.61.2.183:9086",
+    "凤凰中文,http://8.8.8.8:8/rtp/239.61.2.184:9092",
+]
+
+
+results = []
+channel = []
+urls_all = []
+resultsx = []
+resultxs = []
+error_channels = []
+
+for url in urls:
+    url_0 = str(base64.b64encode((f'"Server: udpxy" && city="{url}" && asn="4134"').encode("utf-8")), "utf-8")
+    url_64 = f'https://fofa.info/result?qbase64={url_0}'
+    print(url_64)
+    try:
+        response = requests.get(url_64, headers=headers, timeout=15)
+        page_content = response.text
+        print(f" {url}  访问成功")
+        pattern = r'href="(http://\d+\.\d+\.\d+\.\d+:\d+)"'
+        page_urls = re.findall(pattern, page_content)
+        for urlx in page_urls:
+            try:
+                response = requests.get(url=urlx + '/status', timeout=1)
+                response.raise_for_status()  # 返回状态码不是200异常
+                page_content = response.text
+                pattern = r'class="proctabl"'
+                page_proctabl = re.findall(pattern, page_content)
+                if page_proctabl:
+                    urls_all.append(urlx)
+                    print(f"{urlx} 可以访问")
+
+            except requests.RequestException as e:
+                pass
+    except:
+        print(f"{url_64} 访问失败")
+        pass
+
+urls_all = set(urls_all)  # 去重得到唯一的URL列表
+for urlx in urls_all:
+    channel = [f'{name},{url.replace("http://8.8.8.8:8", urlx)}' for name, url in
+               [line.strip().split(',') for line in channelsx]]
+    results.extend(channel)
+            
+results = sorted(results)
+# with open("ph.txt", 'w', encoding='utf-8') as file:
+#     for result in results:
+#         file.write(result + "\n")
+#         print(result)
+
+# 定义工作线程函数
+def worker():
+    while True:
+        result = task_queue.get()
+        channel_name, channel_url = result.split(',', 1)
+        try:
+            response = requests.get(channel_url, stream=True, timeout=3)
+            if response.status_code == 200:
+                result = channel_name, channel_url
+                resultsx.append(result)
+                numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+                print(
+                    f"可用频道：{len(resultsx)} , 不可用频道：{len(error_channels)} 个 , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
+            else:
+                error_channels.append(result)
+                numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+                print(
+                    f"可用频道：{len(resultsx)} 个 , 不可用频道：{len(error_channels)} , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
+        except:
+            error_channels.append(result)
+            numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+            print(
+                f"可用频道：{len(resultsx)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
+
+        # 标记任务完成
+        task_queue.task_done()
+
+
+# 创建多个工作线程
+num_threads = 20
+for _ in range(num_threads):
+    t = threading.Thread(target=worker, daemon=True)
+    t.start()
+
+# 添加下载任务到队列
+for result in results:
+    task_queue.put(result)
+
+# 等待所有任务完成
+task_queue.join()
+
+
+def channel_key(channel_name):
+    match = re.search(r'\d+', channel_name)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
+
+for resulta in resultsx:
+    channel_name, channel_url = resulta
+    resultx = channel_name, channel_url
+    resultxs.append(resultx)
+
+# 对频道进行排序
+resultxs.sort(key=lambda x: channel_key(x[0]))
+# now_today = datetime.date.today()
+
+result_counter = 20  # 每个频道需要的个数
+
+with open("ph.txt", 'w', encoding='utf-8') as file:
+    channel_counters = {}
+    file.write('港澳频道,#genre#\n')
+    for result in resultxs:
+        channel_name, channel_url = result
+        if '凤凰' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    
+
 # 合并自定义频道文件内容
 file_contents = []
-file_paths = ["hb.txt","he.txt","bj.txt","hn.txt","GAT.txt","gat2.txt","sport.txt"]  # 替换为实际的文件路径列表
+file_paths = ["hb.txt","he.txt","bj.txt","hn.txt","ph.txt","GAT.txt","gat2.txt","sport.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
@@ -1039,6 +1191,7 @@ os.remove("hb.txt")
 os.remove("he.txt")
 os.remove("bj.txt")
 os.remove("hn.txt")
+os.remove("ph.txt")
 os.remove("DIYP-v4.txt")
 os.remove("GAT.txt")
 os.remove("HK.txt")
